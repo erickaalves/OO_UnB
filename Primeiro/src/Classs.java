0078102267a8506f37e7e2da9fbcc6a138add3e7
erickaalves <erickdosantos53@gmail.com>
2022-10-31 17:26:42 -0300

    Classe nova slide aula 01

---
+++ Primeiro/src/Classs.java
@@ -0,0 +1,19 @@
+/* Comentarios com mais de uma linha sao feitos assim
+	 * Evite usar acentos no seu programa
+	 * Blocos de instrucoes sao iniciados com { e finalizados com }
+	 * Nomes de classes iniciam com letra maiuscula
+	 */
+	
+public class Classs{
+	//O metodo main e responsavel pela execucao
+	public static void main ( String [] args ) {
+	/* Strings precisam vir entre aspas
+	 * Nomes de variaveis iniciam com letra minuscula
+	 * Java e case sensitive, ou seja, str e diferente de sTR
+	 */
+	String str = (" Hello ␣ World ");
+	System . out . println ( str );
+	// Uma instrucao deve ser encerrada com ;
+	 }
+	 }
+
