f350ab6e06c1de8d76585fe8fe4677df3498eb7a
erickaalves <erickdosantos53@gmail.com>
2022-11-23 16:26:04 -0300

    testes de classes, objetos e metodos

--- Primeiro/src/MeuPrimeiroPrograma.java
+++ Primeiro/src/MeuPrimeiroPrograma.java
@@ -1,6 +1,7 @@
-public class MeuPrimeiroPrograma {
 
-	public static void main(String[] args) {
+public class MeuPrimeiroPrograma{  /*classe*/
+
+	public static void main(String[] args) {  /*metodo*/
 		System.out.print("Meu primeiro programa Java");
 	}
 
---
+++ controledeorcamento/src/controledeorcamento/CursoemVideo.java
@@ -0,0 +1,9 @@
+package controledeorcamento;
+
+public class CursoemVideo {
+
+	public static void main(String[] args) {
+		System.out.print("Boa noite Larissa, minha bela princesa que tanto amo");
+	}
+
+}
